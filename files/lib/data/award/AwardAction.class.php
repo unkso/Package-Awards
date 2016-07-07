08327fcc9065a42683053d90a8a28d66a31b09ad
Christopher Mühl <christopher@padarom.io>
2016-07-07 15:48:02 +0200

    Add award preview templates

--- files/lib/data/award/AwardAction.class.php
+++ files/lib/data/award/AwardAction.class.php
@@ -2,12 +2,15 @@
 
 use wcf\data\AbstractDatabaseObjectAction;
 use wcf\data\IToggleAction;
+use wcf\system\WCF;
 
 class AwardAction extends AbstractDatabaseObjectAction implements IToggleAction
 {
     protected $className = 'wcf\data\award\AwardEditor';
 
     protected $permissionsDelete = ['admin.clan.award.canManageAwards'];
+
+    protected $allowGuestAccess = ['getAwardPreview', 'getIssuedAwardPreview'];
 
     protected $permissionsUpdate = ['admin.clan.award.canManageAwards'];
 
@@ -20,6 +23,42 @@ class AwardAction extends AbstractDatabaseObjectAction implements IToggleAction
                 'isDisabled' => $award->isDisabled ? 0 : 1,
             ]);
         }
+    }
+
+    public function getAwardPreview()
+    {
+        $award = null;
+
+        WCF::getTPL()->assign([
+            'award' => $award,
+        ]);
+
+        return [
+            'template' => WCF::getTPL()->fetch('awardPreview'),
+        ];
+    }
+
+    public function getIssuedAwardPreview()
+    {
+        $award = null;
+
+        WCF::getTPL()->assign([
+            'award' => $award,
+        ]);
+
+        return [
+            'template' => WCF::getTPL()->fetch('issuedAwardPreview'),
+        ];
+    }
+
+    public function validateGetAwardPreview()
+    {
+        return true;
+    }
+
+    public function validateGetIssuedAwardPreview()
+    {
+        return true;
     }
 
     public function validateToggle()
